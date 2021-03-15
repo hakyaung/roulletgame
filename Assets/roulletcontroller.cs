76bf1e932784e662e821b3c78b85fb70ffa15faa
hakyaung <80668186+hakyaung@users.noreply.github.com>
2021-03-15 15:24:06 +0900

    ..

---
+++ Assets/roulletcontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roulletcontroller : MonoBehaviour
+{
+    float rotspeed = 0;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotspeed = 10.0f;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.rotspeed = 0;
+        }
+        transform.Rotate(0, 0, this.rotspeed);
+        this.rotspeed *= 0.978f;
+    }
+}
